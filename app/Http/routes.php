a86f8640d32ef6fb7c17364e8ecece0ff8f8d820
ThaeThinzarWai <thaetinzarwai@gmail.com>
2016-02-07 02:42:33 +0700

    Making some requirements implementation
    - Created the models
    - Created the migrations and seeds
    - Create our REST routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,4 +20,7 @@ Route::controllers([
 	'password' => 'Auth\PasswordController',
 ]);*/
 
-Route::get('/{name?}', 'MyController@index');
+Route::resource('makers', 'MakerController', ['except' => ['create', 'edit']]);
+Route::resource('vehicles', 'VehicleController', ['only' => ['index']]);
+
+Route::resource('makers.vehicles', 'MakerVehiclesController', ['except' => ['edit', 'create']]);
